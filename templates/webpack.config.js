b3f0df70f368d97b39a9cb3d60df6ae1c23967b3
Hartorn <hartorn.github@gmail.com>
2018-07-01 14:31:35 +0200

    fix: Fix minimizer, fix production build

--- templates/webpack.config.js
+++ templates/webpack.config.js
@@ -3,14 +3,15 @@ const {prepareEntries} = require('@thc/webpack-react/lib/utils');
 //const path = require('path');
 
 module.exports = (processEnv, argv) => {
-    const config = configFunc(processEnv, argv);
+    const env = processEnv || process.env;
+    const config = configFunc(env, argv);
 
     // Add your entries and anything else needed
     // https://webpack.js.org/configuration/entry-context/#entry
     // https://webpack.js.org/configuration/resolve/#resolve-modules
     // https://webpack.js.org/configuration/resolve/#resolve-alias
     // https://webpack.js.org/configuration/entry-context/#context
-    config.entry.main = prepareEntries(processEnv, ['./src/app.js']);
+    config.entry.main = prepareEntries(env, ['./src/app.js']);
 
     return config;
 };
