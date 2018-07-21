be8b29f1e04cb45ab8422f05a53b00a1ed8a503d
Hartorn <bazirehoussin@gmail.com>
2018-07-21 17:39:02 +0200

    chore : Fixing version, config, etc

--- jest.config.js
+++ jest.config.js
@@ -140,9 +140,7 @@ module.exports = {
     testMatch: ["**/test/**/*.js?(x)"],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-    // testPathIgnorePatterns: [
-    //   "\\\\node_modules\\\\"
-    // ],
+    testPathIgnorePatterns: ["\\\\node_modules\\\\", ".history"],
 
     // The regexp pattern Jest uses to detect test files
     // testRegex: "",
