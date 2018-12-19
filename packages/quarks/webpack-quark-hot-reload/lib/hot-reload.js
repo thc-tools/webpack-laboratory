5d9aea75d7b526177740c76000a6dd80899b9420
Hartorn <bazirehoussin@gmail.com>
2018-12-19 02:51:55 +0100

    fix(webpack-quark-hot-reload): fixing non-stringified values for define plugin

--- packages/quarks/webpack-quark-hot-reload/lib/hot-reload.js
+++ packages/quarks/webpack-quark-hot-reload/lib/hot-reload.js
@@ -10,14 +10,17 @@ module.exports = blockConfig => (processEnv, argv) => argConfig => {
     };
 
     const defaultServerConf = {
-        __DEV_SERVER_PROTOCOL__: 'http',
-        __DEV_SERVER_HOST__: 'localhost',
-        __DEV_SERVER_PORT__: '3000',
-        __DEV_SERVER_SUBDOMAIN__: ''
+        __DEV_SERVER_PROTOCOL__: "http",
+        __DEV_SERVER_HOST__: "localhost",
+        __DEV_SERVER_PORT__: 3000,
+        __DEV_SERVER_SUBDOMAIN__: ""
     };
 
     const mergedConf = safeMerge(defaultConf, blockConfig);
     mergedConf.serverConfig = safeMerge(defaultServerConf, mergedConf.serverConfig);
+    mergedConf.serverConfig.__DEV_SERVER_PROTOCOL__ = JSON.stringify(mergedConf.serverConfig.__DEV_SERVER_PROTOCOL__);
+    mergedConf.serverConfig.__DEV_SERVER_HOST__ = JSON.stringify(mergedConf.serverConfig.__DEV_SERVER_HOST__);
+    mergedConf.serverConfig.__DEV_SERVER_SUBDOMAIN__ = JSON.stringify(mergedConf.serverConfig.__DEV_SERVER_SUBDOMAIN__);
 
     const config = ensureConfig(argConfig);
 
