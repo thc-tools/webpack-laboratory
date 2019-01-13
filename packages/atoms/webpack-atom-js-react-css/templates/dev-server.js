f3f19e25026d4d088f521afb9928d3aaee6f9845
Hartorn <bazirehoussin@gmail.com>
2019-01-13 21:33:43 +0100

    chore(webpack-atom-js-react-css): updating templates and dependencies

--- packages/atoms/webpack-atom-js-react-css/templates/dev-server.js
+++ packages/atoms/webpack-atom-js-react-css/templates/dev-server.js
@@ -1,4 +1,4 @@
-const devServer = require("@thc/webpack-atom-js-react-css/lib/dev-server");
+const devServer = require("@thc/webpack-quark-hot-reload/lib/dev-server");
 const webpackConf = require("./webpack.config.js");
 
 const customConfig = {};
