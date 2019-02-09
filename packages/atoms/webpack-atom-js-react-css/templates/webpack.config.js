641966677c5264f0c7dd784477e8fd2fc042b1d5
Hartorn <bazirehoussin@gmail.com>
2019-02-09 10:53:14 +0100

    fix(webpack-atom-js-react-css): fix variable name

--- packages/atoms/webpack-atom-js-react-css/templates/webpack.config.js
+++ packages/atoms/webpack-atom-js-react-css/templates/webpack.config.js
@@ -49,7 +49,7 @@ module.exports = (processEnv, argv) => {
             }
         }),
         handleCss({ extractCss: !hotReload }),
-        handleAssets({ defaultExclude: [/\.ejs$/, /\.jsx?$/, /\.css$/, /\.json$/] }),
+        handleAssets({ defaultsExclude: [/\.ejs$/, /\.jsx?$/, /\.css$/, /\.json$/] }),
         configEntries({
             polyfill: ["@babel/polyfill", /*Needed for Opera Mini*/ "whatwg-fetch"],
             entries: { main: "./src/app.js" },
