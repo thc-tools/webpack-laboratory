50a54e78ac15ece533bfe7908a188b588afac72d
Hartorn <bazirehoussin@gmail.com>
2019-02-23 13:48:10 +0100

    feat(webpack-atom-js-react-scss): creating new atom for js and scss

---
+++ packages/atoms/webpack-atom-js-react-scss/__tests__/webpack-atom-js-react-scss.test.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const confWebpack = require("../templates/webpack.config");
+let config;
+describe("webpack-atom-js-react-scss", () => {
+    describe("for development purposes", () => {
+        beforeEach(() => {
+            config = confWebpack({ NODE_ENV: "development" });
+        });
+        it("should create a config", () => {
+            expect(config).toBeDefined();
+            // console.log("config: %j", config);
+        });
+    });
+    describe("for production purposes", () => {
+        beforeEach(() => {
+            config = confWebpack({ NODE_ENV: "production" });
+        });
+        it("should create a config", () => {
+            expect(config).toBeDefined();
+            // console.log("config: %j", config);
+        });
+    });
+});
